8fd76b11d8a8dbb1542511948766133d282fb9fa
Cobra El Diablo <cobraeldiablo@yahoo.co.uk>
2011-11-06 15:41:10 +0000

    Version number increase for release viewer 1.6.0.2 to 1.6.2.0

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -35,8 +35,8 @@
 
 const S32 LL_VERSION_MAJOR = 1;
 const S32 LL_VERSION_MINOR = 6;
-const S32 LL_VERSION_PATCH = 0;
-const S32 LL_VERSION_BUILD = 2;
+const S32 LL_VERSION_PATCH = 2;
+const S32 LL_VERSION_BUILD = 0;
 
 const char * const LL_CHANNEL = "Astra";
 
